8af671976d59988ad21991e6192ce1a41ed1479a
dezarintk <azaikina811@mail.ru>
2024-05-18 12:18:01 +0300

    Add files via upload

---
+++ faq/main.js
@@ -0,0 +1,78 @@
+function first() {
+
+    document.getElementById("first_hide").setAttribute("style", "opacity:1; transition: 1s;");
+    
+    document.getElementById("first").setAttribute("style", "display: none");
+    
+    document.getElementById("first_after").setAttribute("style", "display: block");
+    
+    }
+    
+    function first_after() {
+    
+    document.getElementById("first_hide").setAttribute("style", "display: none");
+    
+    document.getElementById("first_after").setAttribute("style", "display: none");
+    
+    document.getElementById("first").setAttribute("style", "display: block");
+    
+    }
+
+    function second() {
+
+        document.getElementById("second_hide").setAttribute("style", "opacity:1; transition: 1s;");
+        
+        document.getElementById("second").setAttribute("style", "display: none");
+        
+        document.getElementById("second_after").setAttribute("style", "display: block");
+        
+        }
+        
+        function second_after() {
+        
+        document.getElementById("second_hide").setAttribute("style", "display: none");
+        
+        document.getElementById("second_after").setAttribute("style", "display: none");
+        
+        document.getElementById("second").setAttribute("style", "display: block");
+        
+        }
+        function second() {
+
+        document.getElementById("second_hide").setAttribute("style", "opacity:1; transition: 1s;");
+        
+        document.getElementById("second").setAttribute("style", "display: none");
+        
+        document.getElementById("second_after").setAttribute("style", "display: block");
+        
+        }
+        
+        function second_after() {
+        
+        document.getElementById("second_hide").setAttribute("style", "display: none");
+        
+        document.getElementById("second_after").setAttribute("style", "display: none");
+        
+        document.getElementById("second").setAttribute("style", "display: block");
+        
+        }
+
+    function third() {
+
+        document.getElementById("third_hide").setAttribute("style", "opacity:1; transition: 1s;");
+        
+        document.getElementById("third").setAttribute("style", "display: none");
+        
+        document.getElementById("third_after").setAttribute("style", "display: block");
+        
+        }
+        
+        function third_after() {
+        
+        document.getElementById("third_hide").setAttribute("style", "display: none");
+        
+        document.getElementById("third_after").setAttribute("style", "display: none");
+        
+        document.getElementById("third").setAttribute("style", "display: block");
+        
+        }
